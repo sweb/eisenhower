a2659f7a69b150f40eed75915122bfbcc5e7204a
Florian Müller <florian@tomueller.de>
2014-06-12 17:57:46 +0200

    Initial commit

---
+++ eisenhower.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Task struct {
+  Title string
+  Description string
+  Important boolean
+  Urgent boolean
+  Status string
+  Created string
+  LastChange
+}
