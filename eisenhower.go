77a8280e83c673f83c460958921665b8bfe28d50
Florian Müller <florian@tomueller.de>
2014-06-12 23:04:42 +0200

    Few modifications

--- eisenhower.go
+++ eisenhower.go
@@ -2,14 +2,35 @@ package main
 
 import (
   "fmt"
+  "time"
 )
 
 type Task struct {
   Title string
   Description string
-  Important boolean
-  Urgent boolean
+  Important bool
+  Urgent bool
   Status string
-  Created string
-  LastChange
+  Created time.Time
+  LastChange time.Time
 }
+
+func (t *Task) quadrant() int32 {
+  switch {
+  case t.Urgent && t.Important:
+    return 1
+  case !t.Urgent && t.Important:
+    return 2
+  case t.Urgent && !t.Important:
+    return 3
+  }
+  return 4
+}
+
+
+func main() {
+  t := &Task{Title: "Test", Description: "Testdesc", Important: true, 
+    Urgent:false, Status: "Collect", Created: time.Now(), LastChange: time.Now()}
+	fmt.Printf("Hello, world.\n" + t.Title)
+}
+
